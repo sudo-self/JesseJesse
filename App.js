16960f0c6ea288d85b9e5566f313738b11768447
Sudo-Self <sudo-self@proton.me>
2023-10-06 05:54:57 -0600

    Add files via upload
    Signed-off-by: Sudo-Self <sudo-self@proton.me>

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>JesseJesse.com</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://timeofday.jessejesse.workers.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           ⏰
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
